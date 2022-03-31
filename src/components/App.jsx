5cf6319c509e52f76b1d990fbdffafdc96d912b4
Esther J Iyege <77330706+Faithesther20@users.noreply.github.com>
2022-03-31 15:08:11 +0100

    completed the main work of the toDo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] =useState(" ")
+  const [list, setList] = useState([]);
+  
+  function updateItem(event){
+    var toDo = event.target.value;
+    setItem(toDo);
+    console.log(item)
+  }
+
+// My Own solution
+//  function handleClick(event){
+     
+//     setList(list.concat(item))
+//     console.log(list)
+//     setItem("")
+//  }
+
+function handleClick(event){
+     
+    setList(prevValue =>
+   { return [...prevValue, item];}
+    )
+ }
+
+  function toDo(activity) {
+  return (
+     <li>{activity}</li>
+  );
+}
+  
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={updateItem} value={item}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map(toDo=> {
+          return (
+           <li>{toDo}</li>
+            )
+           }
+          )
+        }
         </ul>
       </div>
     </div>
